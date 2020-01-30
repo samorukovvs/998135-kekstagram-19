c16bcb04dc78a1dc43fb83fcba597f1f09383b0d
Samorukov Vitaliy <samorukov.v.s@gmail.com>
2020-01-30 06:03:20 +0300

    Added main js to index, created integer randomizer

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+var createRandomInteger = function (beginFrom, endOn) {
+  return Math.floor(beginFrom + Math.random() * (endOn - (beginFrom - 1)));
+};
+
+var createPhotoMeta = function () {
+
+
+};
