e16ac380dcfe63392075a35baf66763da0f52657
Samorukov Vitaliy <samorukov.v.s@gmail.com>
2020-02-17 06:29:48 +0300

    Completed effects switch + saturation

--- js/main.js
+++ js/main.js
@@ -93,6 +93,8 @@ var buttonPhotoBigger = editPhotoForm.querySelector('.scale__control--bigger');
 var scaleControl = editPhotoForm.querySelector('.scale__control--value');
 var photoPreview = editPhotoForm.querySelector('.img-upload__preview');
 
+// Добавлено временно, чтобы не открывать каждый раз
+editPhotoForm.classList.remove('hidden');
 
 // Открытие и закрытие изображения
 var closePhoto = function () {
@@ -139,6 +141,11 @@ buttonPhotoSmaller.addEventListener('click', resizePhotoSmaller);
 // Работа с фильтрами
 var currentPhotoEffect = '';
 var effects = document.querySelector('.img-upload__effects');
+var effectsSlider = document.querySelector('.img-upload__effect-level');
+var effectPin = document.querySelector('.effect-level__pin');
+var effectValue = document.querySelector('.effect-level__value');
+var effectVisualValueDepth = document.querySelector('.effect-level__depth');
+
 var photoEffect = [
   'effects__preview--none',
   'effects__preview--chrome',
@@ -149,31 +156,58 @@ var photoEffect = [
 ];
 var ammountOfEffects = photoEffect.length;
 
-// var effectNonePreview = effects.querySelector('.' + photoEffect.NONE);
-// var effectChromePreview = effects.querySelector('.' + photoEffect.CHROME);
-// var effectSepiaPreview = effects.querySelector('.' + photoEffect.SEPIA);
-
-
-// NONE: 'effects__preview--none',
-//   CHROME: 'effects__preview--chrome',
-//   SEPIA: 'effects__preview--sepia',
-//   MARVIN: 'effects__preview--marvin',
-//   PHOBOS: 'effects__preview--phobos',
-//   HEAT: 'effects__preview--heat'
-var setPhotoEffect = function (effect) {
-  if (currentPhotoEffect !== '') {
-    photoPreview.classList.remove(currentPhotoEffect);
+// Рассчет насыщенности эффекта
+var calculateEffect = function (position, effectName) {
+  switch (effectName) {
+    case 'effects__preview--none':
+      return 'none';
+    case 'effects__preview--chrome':
+      return 'grayscale(' + position + ')';
+    case 'effects__preview--sepia':
+      return 'sepia(' + position + ')';
+    case 'effects__preview--marvin':
+      return 'invert(' + position * 100 + '%)';
+    case 'effects__preview--phobos':
+      return 'blur(' + Math.floor(position * 3) + 'px)';
+    case 'effects__preview--heat':
+      return 'brightness(' + ((position * 2) + 1) + ')';
   }
-  if (effect !== 'effects__preview--none') {
-    photoPreview.classList.add(effect);
-  }
-  currentPhotoEffect = effect;
 };
-
+// Установка насыщенности эффекта
+var setEffectSaturation = function (value) {
+  effectPin.style.left = (value * 100) + '%';
+  effectVisualValueDepth.style.width = (value * 100) + '%';
+  effectValue.setAttribute('value', (value * 100) + '%');
+  photoPreview.style.filter = calculateEffect(value, currentPhotoEffect);
+};
+// Выбор эффекта
+var setPhotoEffect = function (effect) {
+  for (i = 0; i < photoEffect.length; i++) {
+    if (effect.includes(photoEffect[i])) {
+      if (currentPhotoEffect !== '') {
+        photoPreview.classList.remove(currentPhotoEffect);
+      }
+      if (photoEffect[i] !== 'effects__preview--none' & currentPhotoEffect === 'effects__preview--none') {
+        effectsSlider.classList.remove('hidden');
+      }
+      if (photoEffect[i] === 'effects__preview--none') {
+        effectsSlider.classList.add('hidden');
+      }
+      photoPreview.classList.add(photoEffect[i]);
+      currentPhotoEffect = photoEffect[i];
+    }
+  }
+  setEffectSaturation(1);
+};
+setPhotoEffect('effects__preview--none');
 var effectPreview = [];
 for (i = 0; i < ammountOfEffects; i++) {
   effectPreview[i] = effects.querySelector('.' + photoEffect[i]);
-  effectPreview[i].addEventListener('click', function () {
-    setPhotoEffect(photoEffect[i]);
+  effectPreview[i].addEventListener('click', function (evt) {
+    setPhotoEffect(evt.target.classList.value);
   });
 }
+
+effectPin.addEventListener('mouseup', function () {
+  setEffectSaturation(0.5);
+});
