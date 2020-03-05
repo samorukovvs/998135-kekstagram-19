0e2a8a262beb0834cd024c5bd158fdb708d18f30
Samorukov Vitaliy <samorukov.v.s@gmail.com>
2020-03-05 05:47:59 +0300

    Completed discussed sort

--- js/gallery.js
+++ js/gallery.js
@@ -5,6 +5,8 @@
   var imgFilter = document.querySelector('.img-filters');
   var defaultSortButton = document.querySelector('#filter-default');
   var randomSortButton = document.querySelector('#filter-random');
+  var discussedSortButton = document.querySelector('#filter-discussed');
+
 
   var fillPhotoTemplate = function (template, photo) {
     var newPhoto = document.querySelector(template).content.cloneNode(true);
@@ -28,6 +30,30 @@
     fragmentPlace.appendChild(fragmentToRender);
   };
 
+  var renderRandomGallery = function() {
+    var randomPicturesIndex = window.utils.createRandomArray(0, window.photosMeta.length-1, RANDOM_GALLERY_SIZE);
+    var randomPictures = [];
+    for (var i = 0; i < randomPicturesIndex.length; i++) {
+      randomPictures.push(window.photosMeta[randomPicturesIndex[i]]);
+    }
+    window.gallery.renderGallery(randomPictures, '.pictures', '#picture', RANDOM_GALLERY_SIZE);
+  }
+
+  var renderDiscussedGallery = function() {
+    var discussedPictures = window.photosMeta.slice();
+
+    discussedPictures.sort(function (img1, img2) {
+      if(img1.comments.length > img2.comments.length) {
+        return -1;
+      }
+      else if(img1.comments.length < img2.comments.length) {
+        return 1;
+      }
+      else return 0;
+      });
+    window.gallery.renderGallery(discussedPictures, '.pictures', '#picture', DEFAULT_GALLERY_SIZE);
+  }
+
   var removeGallery = function () {
     document.querySelectorAll('.picture').forEach(picture => picture.remove());;
   };
@@ -44,21 +70,20 @@
   defaultSortButton.addEventListener('click',  function() {
     makeButtonActive(defaultSortButton);
     renderGallery(window.photosMeta, '.pictures', '#picture', DEFAULT_GALLERY_SIZE);
-    console.log(window.photosMeta);
   });
 
   randomSortButton.addEventListener('click', function() {
     makeButtonActive(randomSortButton);
     removeGallery();
-    var randomPicturesIndex = window.utils.createRandomArray(0, window.photosMeta.length-1, RANDOM_GALLERY_SIZE);
-    var randomPictures = [];
-    for (var i = 0; i < randomPicturesIndex.length; i++) {
-      randomPictures.push(window.photosMeta[randomPicturesIndex[i]]);
-    }
-    window.gallery.renderGallery(randomPictures, '.pictures', '#picture', RANDOM_GALLERY_SIZE);
-    randomPictures = [];
-    console.log(randomPictures);
+    renderRandomGallery();
   });
+
+  discussedSortButton.addEventListener('click', function() {
+    makeButtonActive(discussedSortButton);
+    removeGallery();
+    renderDiscussedGallery();
+  });
+
 
 
 
